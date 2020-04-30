4450ff26ef1da12b3d391d9914d39b24a0f6c177
Kennan Smith <kennan@watermarkadvertising.net>
2020-04-29 18:31:15 -0600

    removed the footer because there is no footer in the design

--- src/components/layout/AngledLayout/Footer/VisionFooter.js
+++ src/components/layout/AngledLayout/Footer/VisionFooter.js
@@ -1,55 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import { Container, Row, Col } from "react-bootstrap";
-import { graphql, useStaticQuery } from "gatsby";
-import { CtflLogo } from "wmk-lib";
 
 const Wrap = styled.footer`
-  background: #2c2b2a;
-  padding: 3vw 0;
-  position: fixed;
-  bottom: 0;
   width: 100%;
-  .center {
-      display: flex;
-      justify-content: center;
-      align-content: center;
-  }
 `;
 
 const VisionFooter = () => {
-  const { options } = useStaticQuery(graphql`
-    {
-      options: contentfulGlobal {
-        logo {
-          file {
-            contentType
-            url
-          }
-          fixed(width: 267, height: 67) {
-            ...GatsbyContentfulFixed
-          }
-          title
-        }
-      }
-    }
-  `);
-  const { logo } = options;
   return (
     <Wrap>
-      <Container>
-        <Row>
-          <Col className="center">
-            <CtflLogo
-              contentType={logo.file.contentType}
-              fixed={logo.fixed}
-              alt={logo.title}
-              target="_self"
-              to={"https://visiongraphics-inc.com/"}
-            />
-          </Col>
-        </Row>
-      </Container>
+
     </Wrap>
   );
 };
