0c331cc0f803f2376e98d6eb33eef4f77976cd19
Kennan Smith <kennan@watermarkadvertising.net>
2020-06-22 15:22:38 -0600

    Del gave me a couple GTM scripts to add and I added them via react helmet

--- src/components/layout/AngledLayout/AngledLayout.js
+++ src/components/layout/AngledLayout/AngledLayout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types";
 // import { useStaticQuery, graphql } from "gatsby";
 import { MainLayout } from "wmk-lib";
 import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
 
 import VisionHeader from "./Header/VisionHeader";
 import VisionFooter from "./Footer/VisionFooter";
@@ -27,6 +28,20 @@ const Wrap = styled.div`
 const Layout = ({ children }) => {
   return (
     <Wrap>
+      <Helmet>
+        <!-- Google Tag Manager -->
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T2XP682');</script>
+        <!-- End Google Tag Manager -->
+
+        <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2XP682"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+      </Helmet>
       <MainLayout
         Header={() => <VisionHeader />}
         Footer={() => <VisionFooter />}
