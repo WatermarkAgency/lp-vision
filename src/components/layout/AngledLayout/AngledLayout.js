9aca8cc1af39df958a1f110b5333f519c26c05ea
Kennan Smith <kennan@watermarkadvertising.net>
2020-06-22 15:28:00 -0600

    commented out comments - hopefully the netlify build will work now

--- src/components/layout/AngledLayout/AngledLayout.js
+++ src/components/layout/AngledLayout/AngledLayout.js
@@ -29,18 +29,18 @@ const Layout = ({ children }) => {
   return (
     <Wrap>
       <Helmet>
-        <!-- Google Tag Manager -->
+        {/* <!-- Google Tag Manager --> */}
           <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-T2XP682');</script>
-        <!-- End Google Tag Manager -->
+        {/* <!-- End Google Tag Manager --> */}
 
-        <!-- Google Tag Manager (noscript) -->
+        {/* <!-- Google Tag Manager (noscript) --> */}
           <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2XP682"
           height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
+        {/* <!-- End Google Tag Manager (noscript) --> */}
       </Helmet>
       <MainLayout
         Header={() => <VisionHeader />}
