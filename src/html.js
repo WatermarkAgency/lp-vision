9d7b997fc9ef37d2b3ae8868f2ccd6c9b34c08f8
Kennan Smith <kennan@watermarkadvertising.net>
2020-06-22 16:03:54 -0600

    adjusted script tags for jsx - i think this is finally the fix i need to get this stupid thing working

--- src/html.js
+++ src/html.js
@@ -12,6 +12,26 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+                {/* <!-- Google Tag Manager --> */}
+                <script dangerouslySetInnerHTML={{
+                  __html: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-T2XP682');
+                  `
+                }} />
+        {/* <!-- End Google Tag Manager --> */}
+
+        {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript dangerouslySetInnerHTML={{
+            __html: `
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2XP682"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `
+          }} />
+        {/* <!-- End Google Tag Manager (noscript) --> */}
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
