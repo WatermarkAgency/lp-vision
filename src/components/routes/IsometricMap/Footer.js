946e1b0eec230fd9f1749e274c40bf778a278973
Kennan Smith <kennan@watermarkadvertising.net>
2020-07-20 08:19:31 -0600

    0.1.1 - set up code to create the page and pasted in the markup for the page header

---
+++ src/components/routes/IsometricMap/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+ 
+const Wrap = styled.div`
+ 
+`
+ 
+const VisionFooter = props => {
+  return (
+    <Wrap>
+      footer
+    </Wrap>
+  )
+}
+ 
+export default VisionFooter
---
+++ src/node/isoMap.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+import Header from '../components/routes/IsometricMap/Header'
+ 
+const Wrap = styled.div`
+ 
+`
+ 
+const isoMap = props => {
+  return (
+    <Wrap>
+      <Header />
+      iso map page
+    </Wrap>
+  )
+}
+ 
+export default isoMap
