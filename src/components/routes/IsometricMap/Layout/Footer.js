b98b0a9a22ee9c395d8cb3d319fbdea635b0f394
Kennan Smith <kennan@watermarkadvertising.net>
2020-07-23 15:43:21 -0600

    0.1.13 - basic footer has been created - not sure if it's reasonable time-wise to integrate the footer form

--- src/components/routes/IsometricMap/Layout/Footer.js
+++ src/components/routes/IsometricMap/Layout/Footer.js
@@ -1,100 +1,173 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FaFacebookSquare, FaTwitterSquare, FaLinkedin, FaInstagram, FaYoutubeSquare } from 'react-icons/fa'
  
+const visOrange = '#F15C2B';
+
 const Wrap = styled.div`
-  height: 20rem;
+  width: 100%;
+  margin-top: 5rem;
+  .content-wrap {
+    clip-path: polygon(0% 20%, 100% 0%, 100% 100%, 0% 100%);
+    background: url(https://visiongraphics-inc.com/wp-content/themes/vision-graphics-v2/assets/images/footer-bg.jpg) no-repeat bottom center;
+    padding-top: 7rem;
+    padding-bottom: 60px;
+    background-size: cover;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .info-piece {
+      display: flex;
+      margin: 15px 0;
+      .title {
+        width: 200px;
+        text-align: right;
+        margin-top: -5px;
+        margin-right: 15px;
+        font-size: 1.5rem;
+        font-weight: 200;
+      }
+      .info {
+        font-size: 1.1rem;
+        font-weight: 200;
+        a {
+          :hover {
+            text-decoration: none;
+          }
+          color: white;
+          &.map {
+            color: ${visOrange};
+            :hover {
+              color: white;
+            }
+          }
+        }     
+        .socials {
+          display: flex;
+          a {
+            color: ${visOrange};
+            margin-right: 2px;
+            font-size: 1.4rem;
+            :hover {
+              color: white;
+            }
+          }
+        }   
+      }
+    }
+    p {
+      margin: 0;
+    }
+  }
+  .copyright-bar {
+    background: #2A2929;
+    padding: 30px;
+    position: relative;
+    p {
+      color: #898888;
+      font-size: .9rem;
+      font-weight: 200;
+      margin-bottom: 5px;
+      a {
+        color: #898888;
+        :hover {
+          color: white;
+          text-decoration: none;
+        }
+      }
+      :last-of-type {
+        position: absolute;
+        right: 30px;
+        bottom: 30px;
+      }
+    }
+  }
+  @media only screen and (min-width: 2000px) {
+    .content-wrap {
+      clip-path: polygon(0% 50%, 100% 0%, 100% 100%, 0% 100%);
+    }
+  }
+  @media only screen and (min-width: 1350px) and (max-width: 2000px) {
+    .content-wrap {
+      clip-path: polygon(0% 30%, 100% 0%, 100% 100%, 0% 100%);
+    }
+  }
+  @media only screen and (max-width: 800px) {
+    .content-wrap {
+      clip-path: polygon(0% 10%, 100% 0%, 100% 100%, 0% 100%);
+    }
+  }
 `
-
-const crap = `<footer class="footer skew-block" role="contentinfo">
-<div class="inner-footer">
-  <div class="grid-container no-padding grid-x grid-padding-x">
-    <div class="small-12 medium-6 large-6 cell short-info">
-      <a class="footer-logo" href="https://visiongraphics-inc.com"><img src="https://visiongraphics-inc.com/wp-content/themes/vision-graphics-v2/assets/images/footer_logo.png" alt="Vision Graphics Logo" /></a>
-      <ul class="contact-list">
-        <li class="address">
-          <span class="title">Address</span>
-          <span class="detail"><p>5105 E 41st Ave<br />Denver, CO 80216<br />
-            <a href="https://goo.gl/maps/2XjaUyfXRHK2" target="_blank">Map It&nbsp;</a></p>
-          </span>
-        </li>
-        <ul class="media-links">
-          <li>
-            <a href="https://www.facebook.com/visiongraphicsinc/" target="_blank"><i class="fab fa-facebook-square"></i></a>
-          </li>
-          <li>
-            <a href="https://twitter.com/printvgi" target="_blank"><i class="fab fa-twitter-square"></i></a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/company/488282/admin/" target="_blank"><i class="fab fa-linkedin"></i></a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/visiongraphicsinc/" target="_blank"><i class="fab fa-instagram"></i></a>
-           </li>
-           <li>
-            <a href="https://www.youtube.com/channel/UCpEmhh4qrgYw5DRkEYFakEA" target="_blank"><i class="fab fa-youtube-square"></i></a>
-           </li>
-        </ul>
-        {/* <!--.media-links--> */}
-        <li>
-            <span class="title">Phone</span>
-            <a class="detail" href="tel:18008334263">1-800-833-4263</a>
-        </li>
-        <li>
-                    <span class="title">Local</span>
-                    <a class="detail" href="tel:18008334263">303-320-5411</a>
-                </li>
-                                        <li>
-                    <span class="title">Email</span>
-                    <a class="detail" href="mailto:webinfo@visiongraphics-inc.com">webinfo@visiongraphics-inc.com</a>
-                </li>
-                                </ul>
-                    {/* <!--<div class="review-btn-holder">
-        <a href="https://reviewmgr.com/vision-graphics/" class="reviewmgr-button" data-content="Review Us" data-replace="true">Review Us</a>
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.reviewmgr.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document, "script", "reviewmgr-wjs");</script>
-    </div>--> */}
-</div>
-<div class="small-12 medium-6 large-6 cell form-holder">
-    
-    <div class="gf_browser_chrome gform_wrapper" id="gform_wrapper_1"><div id="gf_1" class="gform_anchor" tabindex="-1"></div><form method="post" enctype="multipart/form-data" target="gform_ajax_frame_1" id="gform_1" action="/#gf_1">
-            <div class="gform_heading">
-                <h3 class="gform_title">Get in touch with us</h3>
-                <span class="gform_description"></span>
-            </div>
-            <div class="gform_body"><ul id="gform_fields_1" class="gform_fields top_label form_sublabel_below description_below"><li id="field_1_1" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_1">Name<span class="gfield_required">*</span></label><div class="ginput_container ginput_container_text"><input name="input_1" id="input_1_1" type="text" value="" class="medium" tabindex="20" placeholder="Name*" aria-required="true" aria-invalid="false" /></div></li><li id="field_1_2" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_2">Email<span class="gfield_required">*</span></label><div class="ginput_container ginput_container_email">
-                <input name="input_2" id="input_1_2" type="text" value="" class="medium" tabindex="21" placeholder="Email*" aria-required="true" aria-invalid="false" />
-            </div></li><li id="field_1_3" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_3">Comments<span class="gfield_required">*</span></label><div class="ginput_container ginput_container_textarea"><textarea name="input_3" id="input_1_3" class="textarea medium" tabindex="22" placeholder="Comments*" aria-required="true" aria-invalid="false" rows="10" cols="50"></textarea></div></li>
-                </ul></div>
-<div class="gform_footer top_label"> <input type="submit" id="gform_submit_button_1" class="gform_button button" value="Submit your message" tabindex="23" onclick="if(window[&quot;gf_submitting_1&quot;]){return false;}  window[&quot;gf_submitting_1&quot;]=true;  " onkeypress="if( event.keyCode == 13 ){ if(window[&quot;gf_submitting_1&quot;]){return false;} window[&quot;gf_submitting_1&quot;]=true;  jQuery(&quot;#gform_1&quot;).trigger(&quot;submit&quot;,[true]); }" /> <input type="hidden" name="gform_ajax" value="form_id=1&amp;title=1&amp;description=1&amp;tabindex=20" />
-<input type="hidden" class="gform_hidden" name="is_submit_1" value="1" />
-<input type="hidden" class="gform_hidden" name="gform_submit" value="1" />
-
-<input type="hidden" class="gform_hidden" name="gform_unique_id" value="" />
-<input type="hidden" class="gform_hidden" name="state_1" value="WyJbXSIsImJhOTQwMjFlYjVjNTBjMDYwMzVmYTI0Njc5YmFhZjliIl0=" />
-<input type="hidden" class="gform_hidden" name="gform_target_page_number_1" id="gform_target_page_number_1" value="0" />
-<input type="hidden" class="gform_hidden" name="gform_source_page_number_1" id="gform_source_page_number_1" value="1" />
-<input type="hidden" name="gform_field_values" value="" />
-
-</div>
-            </form>
-            </div>
-    <iframe style="display:none;width:0px;height:0px;" src="about:blank" name="gform_ajax_frame_1" id="gform_ajax_frame_1" title="This iframe contains the logic required to handle Ajax powered Gravity Forms."></iframe>
-    <script type="text/javascript">jQuery(document).ready(function($){gformInitSpinner( 1, 'https://visiongraphics-inc.com/wp-content/plugins/gravityforms/images/spinner.gif' );jQuery('#gform_ajax_frame_1').on('load',function(){var contents = jQuery(this).contents().find('*').html();var is_postback = contents.indexOf('GF_AJAX_POSTBACK') >= 0;if(!is_postback){return;}var form_content = jQuery(this).contents().find('#gform_wrapper_1');var is_confirmation = jQuery(this).contents().find('#gform_confirmation_wrapper_1').length > 0;var is_redirect = contents.indexOf('gformRedirect(){') >= 0;var is_form = form_content.length > 0 && ! is_redirect && ! is_confirmation;var mt = parseInt(jQuery('html').css('margin-top'), 10) + parseInt(jQuery('body').css('margin-top'), 10) + 100;if(is_form){jQuery('#gform_wrapper_1').html(form_content.html());if(form_content.hasClass('gform_validation_error')){jQuery('#gform_wrapper_1').addClass('gform_validation_error');} else {jQuery('#gform_wrapper_1').removeClass('gform_validation_error');}setTimeout( function() { /* delay the scroll by 50 milliseconds to fix a bug in chrome */ jQuery(document).scrollTop(jQuery('#gform_wrapper_1').offset().top - mt); }, 50 );if(window['gformInitDatepicker']) {gformInitDatepicker();}if(window['gformInitPriceFields']) {gformInitPriceFields();}var current_page = jQuery('#gform_source_page_number_1').val();gformInitSpinner( 1, 'https://visiongraphics-inc.com/wp-content/plugins/gravityforms/images/spinner.gif' );jQuery(document).trigger('gform_page_loaded', [1, current_page]);window['gf_submitting_1'] = false;}else if(!is_redirect){var confirmation_content = jQuery(this).contents().find('.GF_AJAX_POSTBACK').html();if(!confirmation_content){confirmation_content = contents;}setTimeout(function(){jQuery('#gform_wrapper_1').replaceWith(confirmation_content);jQuery(document).scrollTop(jQuery('#gf_1').offset().top - mt);jQuery(document).trigger('gform_confirmation_loaded', [1]);window['gf_submitting_1'] = false;}, 50);}else{jQuery('#gform_1').append(contents);if(window['gformRedirect']) {gformRedirect();}}jQuery(document).trigger('gform_post_render', [1, current_page]);} );} );</script><script type="text/javascript"> jQuery(document).bind('gform_post_render', function(event, formId, currentPage){if(formId == 1) {if(typeof Placeholders != 'undefined'){
-            Placeholders.enable();
-        }} } );jQuery(document).bind('gform_post_conditional_logic', function(event, formId, fields, isInit){} );</script><script type="text/javascript"> jQuery(document).ready(function(){jQuery(document).trigger('gform_post_render', [1, 1]) } ); </script>            </div>
-</div>
-</div> <!-- end #inner-footer -->
-<div class="credit-info-holder">
-<div class="grid-container grid-x grid-padding-x">
-<p class="copyright large-9 medium-8 small-12 cell">Copyright © 2020 Vision Graphics. All Rights Reserved.</p> <p class="copyright large-9 medium-8 small-12 cell"><a href="/hipaa" target="_blank">HIPAA Compliance</a></p><a href="/hipaa" target="_blank">
-</a><p class="source-org large-3 medium-4 small-12 cell"><a href="/hipaa" target="_blank">Web Design by </a><a href="https://www.anchorwave.com/">Anchor Wave</a> </p>
-</div>
-</div><!--.credit-info-holder-->
-</footer>`
  
 const VisionFooter = props => {
   return (
     <Wrap>
-      footer
+      <div className="content-wrap">
+        <div className="logo-wrap">
+          <a href="https://visiongraphics-inc.com/">
+            <img src="https://visiongraphics-inc.com/wp-content/themes/vision-graphics-v2/assets/images/footer_logo.png" alt="Vision Graphics Logo" />
+          </a>
+        </div>
+        <div className="info-wrap">
+          <div className="info-piece">
+            <div className="title">
+              <p>Address</p>
+            </div>
+            <div className="info">
+              <p>5105 E 41st Ave</p> 
+              <p>Denver, CO 80216</p>
+              <a className="map" href="https://goo.gl/maps/2XjaUyfXRHK2" target="_blank" rel="noreferrer">Map It</a>
+              <div className="socials">
+                <a href="https://www.facebook.com/visiongraphicsinc/" target="_blank" rel="noreferrer">
+                  <FaFacebookSquare />
+                </a>
+                <a href="https://twitter.com/printvgi" target="_blank" rel="noreferrer">
+                  <FaTwitterSquare />
+                </a>
+                <a href="https://www.linkedin.com/company/488282/admin/" target="_blank" rel="noreferrer">
+                  <FaLinkedin />
+                </a>
+                <a href="https://www.instagram.com/visiongraphicsinc/" target="_blank" rel="noreferrer">
+                  <FaInstagram />
+                </a>
+                <a href="https://www.youtube.com/channel/UCpEmhh4qrgYw5DRkEYFakEA" target="_blank" rel="noreferrer">
+                  <FaYoutubeSquare />
+                </a>
+              </div>
+            </div>
+          </div>  
+          <div className="info-piece">
+            <div className="title">
+              <p>Phone</p>
+            </div>
+            <div className="info">
+              <a href="tel:18008334263" target="_blank" rel="noreferrer">1-800-833-4263</a>
+            </div>
+          </div>  
+          <div className="info-piece">
+            <div className="title">
+              Local
+            </div>
+            <div className="info">
+              <a href="tel:3033205411" target="_blank" rel="noreferrer">303-320-5411</a>
+            </div>
+          </div>  
+          <div className="info-piece">
+            <div className="title">
+              Email
+            </div>
+            <div className="info">
+              <a href="mailto:webinfo@visiongraphics-inc.com" target="_blank" rel="noreferrer">webinfo@visiongraphics-inc.com</a>
+            </div>
+          </div>          
+        </div>
+      </div>
+      <div className="copyright-bar">
+        <p>Copyright © 2020 Vision Graphics. All Rights Reserved.</p>
+        <p><a href="https://visiongraphics-inc.com/hipaa-compliance/" target="_blank" rel="noreferrer">HIPAA Compliance</a></p>
+        <p>Web Design by Anchor Wave</p>
+      </div>
     </Wrap>
   )
 }
