7a2e7078d386b003f5fc1632a27dacc938f45dd8
Kennan Smith <kennan@watermarkadvertising.net>
2020-11-05 11:33:45 -0700

    attempting to get GTM to recognize the tags for the lookbook page

--- src/html.js
+++ src/html.js
@@ -28,8 +28,8 @@ export default function HTML(props) {
         {/* <!-- Google Tag Manager (noscript) --> */}
           <noscript dangerouslySetInnerHTML={{
             __html: `
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2XP682"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2XP682"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
             `
           }} />
         {/* <!-- End Google Tag Manager (noscript) --> */}
