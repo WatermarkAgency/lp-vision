d90b9e4eb05593aa6d7fee7ef6455268ef7da01f
Peter Washington <peter@wmkagency.com>
2020-11-10 10:36:36 -0700

    fixed gatsby imports

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,6 @@
  */
 
 // You can delete this file if you're not using it
-import "./node_modules/bootstrap/dist/css/bootstrap.css";
+require("./node_modules/bootstrap/dist/css/bootstrap.css");
 
 require("./src/fonts/atlanta-font/stylesheet.css");
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,6 +5,6 @@
  */
 
 // You can delete this file if you're not using it
-
+require("./node_modules/bootstrap/dist/css/bootstrap.css");
 
 require("./src/fonts/atlanta-font/stylesheet.css");
